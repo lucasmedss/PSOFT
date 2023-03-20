048ec78c9690006f90ff7d61827db7c512c0931a
Lucas Medeiros <92319332+lucasmedss@users.noreply.github.com>
2023-03-20 09:59:37 -0300

    Adiciona prática 1

---
+++ Prática 1 - Relembrando OO/Produto.java
@@ -0,0 +1,15 @@
+public class Produto {
+    private String nome;
+    private String fabricante;
+    private String id;
+
+    Produto(String nome, String fabricante) {
+        this.nome = nome;
+        this.fabricante = fabricante;
+    }
+
+    public String toString() {
+    
+    }
+
+}
---
+++ Prática 1 - Relembrando OO/RepositoryProduto.java
@@ -0,0 +1,6 @@
+public class RepositoryProduto {
+    private String id;
+    private HashMap<String, Produto> produtos;
+    private String dataValidade;
+
+}
